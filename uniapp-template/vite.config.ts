1bb4463420c4db15e838146a92c4d2793c6bae82
HoltChen <xiaocya565@outlook.com>
2025-09-06 00:17:31 +0800

    docs: 调整uniapp的eslint校验

--- uniapp-template/vite.config.ts
+++ uniapp-template/vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
-import uni from '@dcloudio/vite-plugin-uni'
+import { defineConfig } from 'vite';
+import uni from '@dcloudio/vite-plugin-uni';
+import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [uni()],
-})
+  plugins: [
+    uni(),
+    eslintPlugin({
+      include: ['src/**/*.js', 'src/**/*.vue', 'src/**/*.ts'], // 指定需要检查的文件
+      exclude: ['node_modules/**', 'dist/**'], // 指定不需要检查的文件
+      fix: true, // 是否自动修复
+      cache: false, // 是否启用缓存
+    }),
+  ],
+});
